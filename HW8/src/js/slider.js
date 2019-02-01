c33b15c860315deb6a13609b7ea613bb297f8698
Klopotovskyi <patola@ukr.net>
2019-02-02 00:58:07 +0200

    slider changer (nxt & prev btns)

--- HW8/src/js/slider.js
+++ HW8/src/js/slider.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', function () {
     let prev_slide_btn = document.querySelector('.previous-slide');
     let next_slide_btn = document.querySelector('.next-slide');
     let posters = document.querySelectorAll('.scene-preview img');
+    let lenght_slider = posters.length;
+    let items = document.querySelectorAll('div.show-shedule-item');
     let curentItem = 0;
 
     next_slide_btn.addEventListener('click', function () {
@@ -24,6 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function ShowPoster(curentItem) {
         posters[curentItem].classList.toggle('shown');
+        items[curentItem].classList.toggle('current-shedule-item');
     }
     function HidePoster(curentItem) {
         posters[curentItem].classList.remove('shown');
