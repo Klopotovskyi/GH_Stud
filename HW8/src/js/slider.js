a5a062c09c72c6d3b8cde0605b8188e1524fb2a6
Klopotovskyi <patola@ukr.net>
2019-02-07 17:57:12 +0200

    slider COMPLITED

--- HW8/src/js/slider.js
+++ HW8/src/js/slider.js
@@ -3,32 +3,61 @@ document.addEventListener('DOMContentLoaded', function () {
     let prev_slide_btn = document.querySelector('.previous-slide');
     let next_slide_btn = document.querySelector('.next-slide');
     let posters = document.querySelectorAll('.scene-preview img');
-    let lenght_slider = posters.length;
-    let items = document.querySelectorAll('div.show-shedule-item');
-    let curentItem = 0;
+    let dates = document.querySelectorAll('div.show-shedule-item');
+    let items_desc = document.querySelectorAll('div.item-desc');
+    let curentItem = 2;
+
+    activate(posters, curentItem, 'shown');
+    activate(dates, curentItem, 'current-shedule-item');
+    activate(items_desc, curentItem, 'shown');
+
+    function activate(items, k, className) {
+        for (let i = 0, item; i < items.length; i++) {
+            item = items[i];
+            if (i === k) {
+                item.classList.add(className);
+            } else {
+                item.classList.remove(className);
+            }
+        }
+    }
+
+    function cleaningClass(name, className) {
+        for (let j = 0, item; j < name.length; j++) {
+            item = name[j];
+            item.classList.remove(className);
+        }
+    }
+
+    function showPoster(i) {
+        console.log(i);
+        cleaningClass(posters, 'shown');
+        cleaningClass(dates, 'current-shedule-item');
+        cleaningClass(items_desc, 'shown');
+        posters[i].classList.toggle("shown");
+        dates[i].classList.toggle("current-shedule-item");
+        items_desc[i].classList.toggle("shown");
+    }
 
     next_slide_btn.addEventListener('click', function () {
-        HidePoster(curentItem);
         curentItem++;
         if (curentItem >= posters.length) {
             curentItem = 0;
         }
-        ShowPoster(curentItem);
+        showPoster(curentItem);
     });
     prev_slide_btn.addEventListener('click', function () {
-        HidePoster(curentItem);
         curentItem--;
         if (curentItem < 0) {
             curentItem = posters.length - 1;
         }
-        ShowPoster(curentItem);
+        showPoster(curentItem);
     });
+    for (let n = 0, item; n < dates.length; n++) {
+        item = dates[n];
+        item.addEventListener('click', function () {
+            showPoster(n);
+        });
+    }
+});
 
-    function ShowPoster(curentItem) {
-        posters[curentItem].classList.toggle('shown');
-        items[curentItem].classList.toggle('current-shedule-item');
-    }
-    function HidePoster(curentItem) {
-        posters[curentItem].classList.remove('shown');
-    }
-})
